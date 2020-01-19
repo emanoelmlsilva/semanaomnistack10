d19d382f175db5d33b64c25fcbd8ceccdff0ccdb
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2020-01-19 17:23:34 -0300

    create connection with mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/',(request, response) => {
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-rtqow.mongodb.net/week10?retryWrites=true&w=majority',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,    
+})
+
+app.use(express.json());
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção);
+// Body: request.body (Dados para criação ou alteração de um registro);
+
+app.post('/users',(request, response) => {
+    console.log(request.body);
     return response.json({message: 'Hello World'});
 })
 

773a1e631edee0ff960c1d1de0c05ca209adcd22
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2020-01-19 22:49:23 -0300

    created controller to Dev and Search

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção);
+// Body: request.body (Dados para criação ou alteração de um registro);
+
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-rtqow.mongodb.net/week10?retryWrites=true&w=majority',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,    
+})
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);

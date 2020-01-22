293f530a825e0edd7867ec42e7e59a036d5ec86d
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2020-01-22 19:13:30 -0300

    added import cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -17,8 +18,8 @@ mongoose.connect('mongodb+srv://oministack:oministack@cluster0-rtqow.mongodb.net
     useNewUrlParser: true,    
 })
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(3333);
